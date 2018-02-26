3fc2b6243345b0454b7f8d343b5c9305c4c52a0d
Snigdha Verma <36699354+snigdhav03@users.noreply.github.com>
2018-02-27 02:01:43 +0530

    Add files via upload

---
+++ To do List/assets/js/todos.js
@@ -0,0 +1,22 @@
+
+
+$("ul").on("click", "li", function() {
+$(this).toggleClass("done");
+});
+
+$("input").keypress(function(event) {
+	if(event.which===13) {
+	console.log("Enter pressed");
+	var text=$(this).val();
+	$("ul").append("<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i> </span>"+text+"</li>");
+	$(this).val("");
+	}
+});
+
+$("ul").on("click","span", function() {
+	$(this).parent().remove();
+});
+
+$(".fa-plus").click(function() {
+	$("input").fadeToggle();
+})
